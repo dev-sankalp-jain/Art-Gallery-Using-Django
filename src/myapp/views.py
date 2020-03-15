582f4e6a8152acf5d10dd4724a8a302d5a01d6df
sankalp <sankalp.srjain@gmail.com>
2020-03-16 01:21:01 +0530

    login/register/add to cart

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -1,28 +1,45 @@
-from django.shortcuts import render
-from .models import *; 
-# Create your views here.
-def login():
-    pass
-
-def registration():
-    pass
+from django.http import Http404
+from django.shortcuts import get_list_or_404, redirect, render
+from .models import * 
+from django.contrib.auth.models import User, auth
+from datetime import datetime
 
 #  args = list , kwargs = dictionary
 def home_view(request, *args, ** kwargs):
     arts = Art.objects.all()
     return render(request, "index.html", {'arts': arts})
 
+def description_view(request, id , *args, **kwargs):
+    object = Art.objects.get(id=id)
+    context = {
+        'object' : object
+    }
+    print(object)
+    return render (request, 'description.html', context)
 
-def about_us():
+def add(request,id, *args, **kwargs):
+    # print(request.path())
+    print(request.user)
+    if (not request.user.is_authenticated):
+        print("Warining you need to login firdt ")
+        return redirect('../../../login/login')
+    user_obj = User.objects.filter(username = request.user)
+    art_obj = Art.objects.filter(id = id)
+    obj=MyCart.objects.create(user = user_obj[0],art_id=art_obj[0], added_date = datetime.now())
+    obj.save()
+    return redirect('../../../')
+
+def about_us_view(request, *args, **kwargs):
     # about us
-    pass
+    return render(request,"aboutus.html",{})
 
-def cart():
+def cart_view(request, *args, **kwargs):
 
-    pass
+    return render(request,"cart.html",{})
 
-def order():
-    pass
+def order_view(request, *args, **kwargs):
+    
+    return render(request,"order.html")
 
 
 # index page 
